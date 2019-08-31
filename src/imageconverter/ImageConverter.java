6d39183833eb66eab94cc9880c5d66cef7ba1b29
git-davi <davidecasalini2@gmail.com>
2019-08-31 12:48:28 +0200

    ypbpr got some error, ycbcr added

--- src/imageconverter/ImageConverter.java
+++ src/imageconverter/ImageConverter.java
@@ -4,7 +4,6 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBuffer;
 import java.io.File;
 import java.nio.FloatBuffer;
-
 import java.util.function.Function;
 
 import org.jblas.FloatMatrix;
@@ -16,6 +15,15 @@ public class ImageConverter {
 	public final static int Y = 0, Pb = 1, Pr = 2;
 	public final static int RGB_CHANNELS = 3;
 	public final static int YPBPR_CHANNELS = 3;
+	
+	public final static int Cb = 1, Cr = 2;
+	public final static int YCBCR_CHANNELS = 3;
+	
+	
+	private static void normalizeMatricesInPlace(FloatMatrix[] matrices, float value) {
+		for (int i = 0; i < matrices.length; i++) 
+			matrices[i].divi(value);
+	}
 	
 	
 	/*
@@ -60,22 +68,6 @@ public class ImageConverter {
 	}
 		
 	
-	/*
-	 * Convert matrix array to tensor [rows][columns][channels]
-	 */
-	private static float[][][] matrixToTensor(FloatMatrix[] matrices) {
-		float[][][] tensor = new float[matrices[0].rows]
-				[matrices[0].columns]
-						[matrices.length];
-		
-		
-		for (int i = 0; i < matrices[0].rows; i++) 
-			for (int j = 0; j < matrices[0].columns; j++) 
-				for (int k = 0; k < matrices.length; k++)
-					tensor[i][j][k] = matrices[k].get(i, j);
-		
-		return tensor;
-	}
 	
 	/*
 	 * My implementation for RGB to YPbPr conversion. 
@@ -85,10 +77,7 @@ public class ImageConverter {
 	public static FloatMatrix[] convertRGB2YPbPr(FloatMatrix[] rgbImagePixels) {
 		FloatMatrix[] ypbprImagePixels = new FloatMatrix[YPBPR_CHANNELS];
 		
-		// scale matrix
-		for (int i = 0; i < RGB_CHANNELS; i++)  {
-			rgbImagePixels[i] = rgbImagePixels[i].div(255.0f);
-		}
+		normalizeMatricesInPlace(rgbImagePixels, 255);
 		
 		// Y = 0.2126 R + 0.7152 G + 0.0722 B
 		ypbprImagePixels[Y] = rgbImagePixels[R].mul(0.2126f).add(rgbImagePixels[G].mul(0.7152f)).add(rgbImagePixels[B].mul(0.0722f));
@@ -102,7 +91,30 @@ public class ImageConverter {
 		return ypbprImagePixels;
 	}
 
-
+	
+	public static FloatMatrix[] convertRGB2YCbCr(FloatMatrix[] rgbImagePixels) {
+		FloatMatrix[] ypbprImagePixels = new FloatMatrix[YCBCR_CHANNELS];
+		
+		normalizeMatricesInPlace(rgbImagePixels, 256);
+		
+		ypbprImagePixels[Y] = rgbImagePixels[R].mul(65.738f)
+								.add(rgbImagePixels[G].mul(129.057f))
+								.add(rgbImagePixels[B].mul(25.064f))
+								.add(16f);
+		
+		ypbprImagePixels[Cb] = rgbImagePixels[R].mul(-37.945f)
+								.add(rgbImagePixels[G].mul(-74.494f))
+								.add(rgbImagePixels[B].mul(112.439f))
+								.add(128f);
+		
+		ypbprImagePixels[Cr] = rgbImagePixels[R].mul(112.439f)
+								.add(rgbImagePixels[G].mul(-96.154f))
+								.add(rgbImagePixels[B].mul(-18.285f))
+								.add(128f);
+				
+		return ypbprImagePixels;
+	}
+	
 	/*
 	 * This provides the standard routine for color conversion
 	 * Returns the pixel in a tensor shape [width][height][channels]
@@ -124,7 +136,7 @@ public class ImageConverter {
 		rgbImagePixels = createRGBMatricesFast(rgbImage);
 		ypbprImagePixels = colorSpaceConversion.apply(rgbImagePixels);
 		
-		return matrixToTensor(ypbprImagePixels);
+		return ArrayOp.matrixToTensor(ypbprImagePixels);
 	}	
 	
 }
